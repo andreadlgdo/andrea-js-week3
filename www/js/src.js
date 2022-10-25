ab2bd0b447e383f6f4f88ce26feac527a16a0614
andreadelgado <andreadlgdo@hotmail.com>
2022-10-25 12:09:01 +0200

    first commit

--- www/js/src.js
+++ www/js/src.js
@@ -1,1 +1,18 @@
 'use strict';
+
+async function getCharacters(){
+  const response=await fetch('https://rickandmortyapi.com/api/character');
+  const allCharacters = await response.json();
+  return allCharacters;
+
+}
+getCharacters().then(allCharacters => {
+  const characters=allCharacters.results;
+  console.log(allCharacters.results[0]);
+  for(let i=0;i<characters.length/2;i++){
+    let newElement=document.createElement('h2');
+    newElement.textContent=characters[i].name;
+    document.querySelector('section').append(newElement);
+
+  }
+});
