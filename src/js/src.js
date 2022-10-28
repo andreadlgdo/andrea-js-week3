4a5580a1afd393822b84701b844f3b18db9045a2
andreadelgado <andreadlgdo@hotmail.com>
2022-10-28 09:22:59 +0200

    filter one

--- src/js/src.js
+++ src/js/src.js
@@ -21,6 +21,7 @@ async function getCharactersByFilter(type,valor) {
   return results;
 }
 
+
 // eslint-disable-next-line no-unused-vars
 function  filterby(type,valor){
   getCharactersByFilter(type,valor).then(allCharacteres=>{
